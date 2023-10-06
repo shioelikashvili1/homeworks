a27d8e02a5619a740864cc05764b36bd1a2229a3
shioelikashvili1 <elikashvilishota59@gmail.com>
2023-10-06 19:17:40 +0400

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,11 @@
+name = "shio"
+surname = "elikashvili"
+age = 13
+weight = 70
+height = 175
+
+
+
+
+print ("my name is" + " " + name + " " + surname + " " + "i am " + str(age) + " years old " + "and my weight is " + str(weight) + "kg, " + "my height is " + str(height) + "cm")
+       
