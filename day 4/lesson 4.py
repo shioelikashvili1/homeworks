9805b6a04da15678454e5cba8c909e89c118f827
shioelikashvili1 <elikashvilishota59@gmail.com>
2023-10-08 01:58:28 +0400

    fourth commit

---
+++ day 4/lesson 4.py
@@ -0,0 +1,7 @@
+user_name = input("what's yor name: ")
+user_surname = input("what's your surname: ")
+user_age = input("what's your age: ")
+user_mothername = input("what is your mother's name: ")
+user_fathername = input("what is your father's name: ")
+
+print("you are ", float(user_age) + 15, "years old in 15 years later")
