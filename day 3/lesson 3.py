e6fba57f9aa82cd09d5ba93626a5d892f4e7fb4e
shioelikashvili1 <elikashvilishota59@gmail.com>
2023-10-12 23:55:39 +0400

    fifth commit

--- day 3/lesson 3.py
+++ day 3/lesson 3.py
@@ -2,4 +2,3 @@ mother = 50
 me = 13
 
 print(mother + me + 10)
-
---
+++ day 5/womeworke 5.py
@@ -0,0 +1,43 @@
+#make registration column
+user_name= input("enter your name: ")
+
+
+user_surname = input("enter your surname: ")
+
+
+user_nickname = input("enter your nickname: ")
+
+
+user_password = input("enter password 8-24 characters: ")
+
+
+user_confirmpassword = input("confirm your password: ")
+
+
+user_email = input("enter your email: ")
+
+
+user_phonenumber = input("enter your phonenumber: ")
+
+
+user_age = input("enter your age: ")
+                 
+
+user_mothername = input("enter your mother name: ")
+
+
+user_fathername = input("enter your father name: ")
+
+
+#user checking hiss information
+
+print("your accaont information: ")
+
+print("your name is ", user_name)
+print("your surname is ", user_surname)
+print("your nickname is ", user_nickname)
+print("your email is: ", user_email)
+print("your phone number is: ", user_phonenumber)
+print("your age is ", user_age, "y/o")
+print("your mother name is ", user_mothername)
+print("your father name is ", user_fathername)
